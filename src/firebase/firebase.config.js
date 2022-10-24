9f4d52bde0a6b67d22452e80c7dcb88dc8e00199
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-10-25 00:49:53 +0600

    ok

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnL1poytiUjAw0SE50xSDmBvgFLTAnzRU",
+  authDomain: "skill-edge-5f470.firebaseapp.com",
+  projectId: "skill-edge-5f470",
+  storageBucket: "skill-edge-5f470.appspot.com",
+  messagingSenderId: "269825441716",
+  appId: "1:269825441716:web:a75f517f03c51520123ce1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
