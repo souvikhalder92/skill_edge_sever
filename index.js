c1929e0317dcb6cf844829f34aab8831bad880ff
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-10-26 01:59:21 +0600

    ok

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const courses = require('./data/courses.json');
 
@@ -11,7 +14,14 @@ app.get('/',(req,res) =>{
 app.get('/courses',(req,res) =>{
     res.send(courses);
 
-})
+});
+app.get('/courses/:id',(req,res) =>{
+    const id = req.params.id;
+    const selectedCourse = courses.find(n => n.id === id);
+    res.send(selectedCourse);
+
+});
+
 
 app.listen(port,() =>{
     console.log('Skill');
